40ae245ddceb23939a3a722045f40e6bf2008bd5
brgcode <tom.v.mele@gmail.com>
2022-11-06 09:57:07 +0100

    curve continued

---
+++ day1/03/408_curve.py
@@ -0,0 +1,27 @@
+from compas.geometry import Point
+from compas.geometry import Polyline
+from compas.geometry import NurbsCurve
+from compas.utilities import linspace
+from compas_view2.app import App
+from compas_view2.objects import Collection
+
+points = [
+    Point(0, 0, 0),
+    Point(3, 3, 0),
+    Point(6, -6, 3),
+    Point(9, 0, 0),
+]
+curve = NurbsCurve.from_points(points)
+
+frames = []
+for t in linspace(curve.domain[0], curve.domain[1], 100):
+    frames.append(curve.frame_at(t))
+
+viewer = App()
+viewer.view.camera.position = [-6, 0, 6]
+viewer.view.camera.target = [0, 0, 4]
+
+viewer.add(curve.to_polyline(), linewidth=3)
+viewer.add(Polyline(curve.points), show_points=True)
+viewer.add(Collection(frames))
+viewer.show()
