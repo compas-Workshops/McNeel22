859e489a04983e14a27145f5e3064f82d5fe68ed
brgcode <tom.v.mele@gmail.com>
2022-11-09 08:40:46 +0100

    small DEM example updates

--- day2/03/000_stack.py
+++ day2/03/000_stack.py
@@ -1,45 +1,56 @@
+import os
+import compas
 from compas.geometry import Box
 from compas.geometry import Frame
 from compas.geometry import Translation
+from compas_assembly.datastructures import Assembly
 from compas_assembly.datastructures import Block
-from compas_cra.algorithms import assembly_interfaces_numpy
-from compas_cra.datastructures import CRA_Assembly
-from compas_cra.equilibrium import cra_solve
+from compas_assembly.algorithms import assembly_interfaces
 from compas_assembly.viewer import DEMViewer
+from compas_cra.equilibrium import cra_solve
+
+HERE = os.path.dirname(__file__)
+ASSEMBLY = os.path.join(HERE, "two-blocks-assembly.json")
 
 # =============================================================================
 # Geometry
 # =============================================================================
 
-support = Box(Frame.worldXY(), 1, 1, 1)
+box1 = Box(Frame.worldXY(), 1, 1, 1)
 
-free1 = support.transformed(Translation.from_vector([0, 0, 1]))
-free1.transform(Translation.from_vector([0.1, 0.3, 0]))
+box2 = box1.transformed(Translation.from_vector([0.1, 0.3, 1]))
 
 # =============================================================================
 # Assembly
 # =============================================================================
 
-assembly = CRA_Assembly()
-assembly.add_block(Block.from_shape(support), node=0)
-assembly.add_block(Block.from_shape(free1), node=1)
-assembly.set_boundary_conditions([0])
+assembly = Assembly()
+assembly.add_block(Block.from_shape(box1), node=0)
+assembly.add_block(Block.from_shape(box2), node=1)
 
 # =============================================================================
 # Interfaces
 # =============================================================================
 
-assembly_interfaces_numpy(assembly, nmax=10, amin=1e-2, tmax=1e-2)
+assembly_interfaces(assembly, nmax=10, amin=1e-2, tmax=1e-2)
+
+# =============================================================================
+# Boundary conditions
+# =============================================================================
+
+assembly.set_boundary_condition(1)
 
 # =============================================================================
 # Equilibrium
 # =============================================================================
 
-cra_solve(assembly, density=1)
+cra_solve(assembly)
 
 # =============================================================================
 # Export
 # =============================================================================
+
+compas.json_dump(assembly, ASSEMBLY)
 
 # =============================================================================
 # Viz
